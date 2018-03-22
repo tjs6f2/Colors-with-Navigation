afe04a716c8d38f46bfa91c30500bba32db2ab74
Thomas Schultz <tjschultz@mwc-039008.dhcp.missouri.edu>
2018-03-22 17:09:44 -0500

    Added navigation between cells

--- Colors/ColorsViewController.swift
+++ Colors/ColorsViewController.swift
@@ -19,6 +19,8 @@ class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDa
                   Color(name:"purple", uiColor: UIColor.purple),
                   Color(name:"brown", uiColor: UIColor.brown)]
 
+    @IBOutlet weak var colorsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -46,15 +48,17 @@ class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         return cell
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.isSelected = false
     }
-    */
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? ColorDetailViewController,
+            let row = colorsTableView.indexPathForSelectedRow?.row
+        {
+            destination.color = colors[row]
+        }
+    }
 
 }
